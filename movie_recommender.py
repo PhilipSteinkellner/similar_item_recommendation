e3874a646190d16c6bd12f63783d18ad08558fbd
PhilipSteinkellner <p.steinkellner@hotmail.com>
2019-06-04 15:13:35 +0200

    inital commit

---
+++ movie_recommender.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+similar_movies = []
+
+
+def same_actors(movie_id):
+
+    return similar_movies
+
+
+def same_director(movie_id):
+
+    return similar_movies
+
+
+def similar_plot_description(movie_id):
+
+    return similar_movies
+
+
+def same_genres(movie_id):
+
+    return similar_movies
+
+
+def similar_movie_cover(movie_id):
+
+    return similar_movies
