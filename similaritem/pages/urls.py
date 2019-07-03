32e316f8019dfa115dc69e7d3abe92d06633d427
raphaelduhs <raphael.duhs@gmail.com>
2019-07-03 16:25:33 +0200

    Ux is getting there plus first algorithm is working. TODO: User input to get certain movie

--- similaritem/pages/urls.py
+++ similaritem/pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 
     path('result', views.result_view, name='result'),
-    path('', views.initial_view, name='home')
+    path('', views.initial_view, name='home'),
+    path('mh', views.movie_view, name='movies')
 
 ]
